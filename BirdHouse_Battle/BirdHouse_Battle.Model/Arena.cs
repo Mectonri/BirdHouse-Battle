3271f6b94e524c5319328cec1cfd68ccb3ebd1a0
Mectonri <thibault.leclerc013@gmail.com>
2018-10-15 11:47:36 +0200

    Adding search for nearest ennemy

--- BirdHouse_Battle/BirdHouse_Battle.Model/Arena.cs
+++ BirdHouse_Battle/BirdHouse_Battle.Model/Arena.cs
@@ -6,73 +6,106 @@ namespace BirdHouse_Battle.Model
 {
     class Arena
     {
-        //readonly Dictionary<string, Teams> _teams;
-        public Arena()
+        readonly Dictionary<string, Teams> _teams;
+        readonly int _height;
+        readonly int _width;
+        public Arena(int heigth, int width)
         {
-            //_teams = new Dictionary<string, Teams>();
+            _teams = new Dictionary<string, Teams>();
+            _height = heigth;
+            _width = width;
         }
 
 
 
 
-        //public Team CreateTeam()
-        //{
+        public Team CreateTeam(string name)
+        {
+            if (_teams.ContainsKey(name)) throw new ArgumentException("A team with this name already exists.", nameof(name));
 
-        //}
-        //public Team FindTeam()
-        //{
+            Team team = new Team(this, name);
+            _teams.Add(name, team);
+            return team;
+        }
 
-        //}
-        //internal List<Team> GetTeams()
-        //{
+        public Team FindTeam(string name)
+        {
+            Team result;
+            _teams.TryGetValue(name, out result);
+            Team cat = result as Team;
+            return Team;
+        }
 
-        //}
 
 
 
 
-        //public Unit CreateUnit()
-        //{
-        //    Unit unit = new Unit();
-        //    return unit;
-        //}
+        
         //public void SpawnUnit()
         //{
 
         //}
 
 
+        public Unit NearestEnnemy (Unit unit)
+        {
+            int x = unit.position.x;
+            int y = unit.position.y;
+            double distance=0;
+            Unit ennemyUnit;
+            foreach (Team team in _teams)
+            {
+                foreach (Unit unitBis in _teams._units)
+                {
+                    if (distance == 0)
+                    {
+                        distance = Math.Sqrt(Math.Pow(x - unitBis.position.x, 2) + Math.Pow(y - unitBis.position.y, 2));
+                        ennemyUnit = unitBis;
+                    }
+                    else
+                    {
+                        if (distance > Math.Sqrt(Math.Pow(x - unitBis.position.x, 2) + Math.Pow(y - unitBis.position.y, 2)))
+                        {
+                            ennemyUnit = unitBis;
+                        }
+                    }  
+                }
+            }
+            return ennemyUnit;
+        }
+
+        
+
+
+
+        //internal vector nextrandomposition()
+        //{
+        //    return new vector(nextrandomdouble(-1.0, 1.0), nextrandomdouble(-1.0, 1.0));
+        //}
+
+        //internal vector nextrandomdirection()
+        //{
+        //    double x = nextrandomdouble(-1.0, 1.0);
+        //    double alpha = math.acos(x);
+        //    double y = math.sin(alpha);
+        //    if (_random.nextdouble() < 0.5) y = y * -1.0;
+        //    return new vector(x, y);
+        //}
+
+        //internal vector nextrandomdirection(vector direction, double alpha)
+        //{
+        //    double beta = nextrandomdouble(-1.0 * alpha, alpha);
+        //    double newx = math.cos(beta + math.acos(direction.x));
+        //    double newy = math.sin(beta + math.asin(direction.y));
+
+        //    vector vector = new vector(newx, newy);
+        //    return vector.multiply(1.0 / vector.magnitude);
+        //}
 
 
         public void Update()
         {
-
+            throw new ArgumentNullException();
         }
-
-
-
-        //internal Vector NextRandomPosition()
-        //{
-        //    return new Vector(NextRandomDouble(-1.0, 1.0), NextRandomDouble(-1.0, 1.0));
-        //}
-
-        //internal Vector NextRandomDirection()
-        //{
-        //    double x = NextRandomDouble(-1.0, 1.0);
-        //    double alpha = Math.Acos(x);
-        //    double y = Math.Sin(alpha);
-        //    if (_random.NextDouble() < 0.5) y = y * -1.0;
-        //    return new Vector(x, y);
-        //}
-
-        //internal Vector NextRandomDirection(Vector direction, double alpha)
-        //{
-        //    double beta = NextRandomDouble(-1.0 * alpha, alpha);
-        //    double newX = Math.Cos(beta + Math.Acos(direction.X));
-        //    double newY = Math.Sin(beta + Math.Asin(direction.Y));
-
-        //    Vector vector = new Vector(newX, newY);
-        //    return vector.Multiply(1.0 / vector.Magnitude);
-        //}
     }
 }
