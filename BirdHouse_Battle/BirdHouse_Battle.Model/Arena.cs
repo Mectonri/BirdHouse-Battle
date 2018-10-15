fcb515f6c73754e75c0150dc3eb219691f155136
Mectonri <thibault.leclerc013@gmail.com>
2018-10-15 13:56:19 +0200

    Adding basic collision
    unit shouldn't be able to sit on top of each other

--- BirdHouse_Battle/BirdHouse_Battle.Model/Arena.cs
+++ BirdHouse_Battle/BirdHouse_Battle.Model/Arena.cs
@@ -6,101 +6,99 @@ namespace BirdHouse_Battle.Model
 {
     class Arena
     {
-        readonly Dictionary<string, Teams> _teams;
+        //readonly Dictionary<string, Teams> _teams;
         readonly int _height;
         readonly int _width;
-        public Arena(int heigth, int width)
+        public Arena()
         {
-            _teams = new Dictionary<string, Teams>();
-            _height = heigth;
-            _width = width;
+            //_teams = new Dictionary<string, Teams>();
+            _height = 100;
+            _width = 100;
         }
 
 
 
 
-        public Team CreateTeam(string name)
-        {
-            if (_teams.ContainsKey(name)) throw new ArgumentException("A team with this name already exists.", nameof(name));
+        //public Team CreateTeam(string name)
+        //{
+        //    if (_teams.ContainsKey(name)) throw new ArgumentException("A team with this name already exists.", nameof(name));
+        //    if (_teams.Count >= 4) throw new Exception("You already have 4 team and cannot create any more");
+        //    Team team = new Team(this, name);
+        //    _teams.Add(name, team);
+        //    return team;
+        //}
 
-            Team team = new Team(this, name);
-            _teams.Add(name, team);
-            return team;
+        //public void RemoveTeam(string name)
+        //{
+        //    if (_teams.ContainsKey(name) == false) throw new ArgumentException("A team with name does not exist", nameof(name));
+
+        //    _teams.Remove(name);
+        //}
+
+        //public Team FindTeam(string name)
+        //{
+        //    Team result;
+        //    _teams.TryGetValue(name, out result);
+        //    Team cat = result as Team;
+        //    return Team;
+        //}
+
+
+
+        //public bool Collision(Unit unit)
+        //{
+        //    int x = unit.position.x;
+        //    int y = unit.position.y;
+        //    bool doesCollide = false;
+        //    foreach (Team team in _teams)
+        //    {
+        //        foreach (Unit unitBis in _teams._units)
+        //        {
+        //            if (x == unitBis.position.x && y == unitBis.position.y && unitBis != unit) doesCollide = true;
+        //        }
+        //    }
+        //    return doesCollide;
+        //}
+
+        public Unit SpawnUnit(Unit unit)
+        {
+            
         }
 
-        public Team FindTeam(string name)
-        {
-            Team result;
-            _teams.TryGetValue(name, out result);
-            Team cat = result as Team;
-            return Team;
-        }
 
 
+        //public Unit NearestEnnemy (Unit unit)
+        //{
+        //    int x = unit.position.x;
+        //    int y = unit.position.y;
+        //    double distance=0;
+        //    Unit ennemyUnit;
+        //    foreach (Team team in _teams)
+        //    {
+        //        foreach (Unit unitBis in _teams._units)
+        //        {
+        //            if (distance == 0)
+        //            {
+        //                distance = Math.Sqrt(Math.Pow(x - unitBis.position.x, 2) + Math.Pow(y - unitBis.position.y, 2));
+        //                ennemyUnit = unitBis;
+        //            }
+        //            else
+        //            {
+        //                if (distance > Math.Sqrt(Math.Pow(x - unitBis.position.x, 2) + Math.Pow(y - unitBis.position.y, 2)))
+        //                {
+        //                    ennemyUnit = unitBis;
+        //                }
+        //            }  
+        //        }
+        //    }
+        //    return ennemyUnit;
+        //}
+
+        
 
 
 
         
-        //public void SpawnUnit()
-        //{
-
-        //}
-
-
-        public Unit NearestEnnemy (Unit unit)
-        {
-            int x = unit.position.x;
-            int y = unit.position.y;
-            double distance=0;
-            Unit ennemyUnit;
-            foreach (Team team in _teams)
-            {
-                foreach (Unit unitBis in _teams._units)
-                {
-                    if (distance == 0)
-                    {
-                        distance = Math.Sqrt(Math.Pow(x - unitBis.position.x, 2) + Math.Pow(y - unitBis.position.y, 2));
-                        ennemyUnit = unitBis;
-                    }
-                    else
-                    {
-                        if (distance > Math.Sqrt(Math.Pow(x - unitBis.position.x, 2) + Math.Pow(y - unitBis.position.y, 2)))
-                        {
-                            ennemyUnit = unitBis;
-                        }
-                    }  
-                }
-            }
-            return ennemyUnit;
-        }
-
-        
-
-
-
-        //internal vector nextrandomposition()
-        //{
-        //    return new vector(nextrandomdouble(-1.0, 1.0), nextrandomdouble(-1.0, 1.0));
-        //}
-
-        //internal vector nextrandomdirection()
-        //{
-        //    double x = nextrandomdouble(-1.0, 1.0);
-        //    double alpha = math.acos(x);
-        //    double y = math.sin(alpha);
-        //    if (_random.nextdouble() < 0.5) y = y * -1.0;
-        //    return new vector(x, y);
-        //}
-
-        //internal vector nextrandomdirection(vector direction, double alpha)
-        //{
-        //    double beta = nextrandomdouble(-1.0 * alpha, alpha);
-        //    double newx = math.cos(beta + math.acos(direction.x));
-        //    double newy = math.sin(beta + math.asin(direction.y));
-
-        //    vector vector = new vector(newx, newy);
-        //    return vector.multiply(1.0 / vector.magnitude);
-        //}
 
 
         public void Update()
