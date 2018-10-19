db49076895f7c7bb44825bff14db13b660f2ebcc
Anthony LASSERRE <lasserre@intechinfo.fr>
2018-10-19 09:21:03 +0200

    Full finish unitTests exept Nearest Enemy
    Need Merge to finish

--- BirdHouse_Battle/BirdHouse_Battle.UnitTests/UnitTests_Unit.cs
+++ BirdHouse_Battle/BirdHouse_Battle.UnitTests/UnitTests_Unit.cs
@@ -16,7 +16,24 @@ namespace BirdHouse_Battle.UnitTests
         [Test]
         public void Die_Null_Context()
         {
-            throw new ArgumentNullException();
+            Arena arena = new Arena();
+            Team team = arena.CreateTeam("red");
+
+            Paladin Pal = new Paladin(team, arena);
+            Gobelin Gob = new Gobelin(team, arena);
+            Archer Arc = new Archer(team, arena);
+
+            Assert.That(Pal.Team, Is.EqualTo(team));
+            Pal.DieNullContext();
+            Assert.That(Pal.Team, Is.EqualTo(null));
+
+            Assert.That(Gob.Team, Is.EqualTo(team));
+            Gob.DieNullContext();
+            Assert.That(Gob.Team, Is.EqualTo(null));
+
+            Assert.That(Arc.Team, Is.EqualTo(team));
+            Arc.DieNullContext();
+            Assert.That(Arc.Team, Is.EqualTo(null));
         }
     }
 }
