6e1bed269f78456d84dc26ec6c9cd3b10cb4ed52
Lexa <saxelsyeguillaume@gmail.com>
2018-10-22 10:56:22 +0200

    Revert "Merge branch 'Develop' into Lexa_remote"
    This reverts commit 4f947f1d60a8aaed91106a49100eef1b20e412dc.

--- BirdHouse_Battle/BirdHouse_Battle.UnitTests/UnitTests_Unit.cs
+++ BirdHouse_Battle/BirdHouse_Battle.UnitTests/UnitTests_Unit.cs
@@ -10,26 +10,6 @@ namespace BirdHouse_Battle.UnitTests
         [Test]
         public void Nearest_Enemy()
         {
-            Arena arena = new Arena();
-            Team team = arena.CreateTeam("red");
-            Team team2 = arena.CreateTeam("blue");
-            Team team3 = arena.CreateTeam("green");
-
-            team.AddArcher(1);
-            team2.AddPaladin(2);
-            team3.AddGobelin(1);
-
-            Unit[] tab = team.Find();// Useless but no other option.
-            Unit[] tab2 = team2.Find();// Useless but no other option.
-            Unit[] tab3 = team3.Find();// Useless but no other option.
-
-            tab[0].Location = new Vector(0, 0);// Can't give real location to the unit.
-
-            tab2[0].Location = new Vector(5, 5);// Can't give real location to the unit.
-            tab2[1].Location = new Vector(7, 7);// Can't give real location to the unit.
-
-            tab3[0].Location = new Vector(8, 8);// Can't give real location to the unit.
-
             throw new ArgumentNullException();
         }
 
